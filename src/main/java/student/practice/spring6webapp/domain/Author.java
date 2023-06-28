91983dde1efa774a17ba2c2e0930fadfe051af5e
jburns6789 <117755326+jburns6789@users.noreply.github.com>
2023-06-28 11:40:22 -0400

    project initialized and pojos created

---
+++ src/main/java/student/practice/spring6webapp/domain/Author.java
@@ -0,0 +1,56 @@
+package student.practice.spring6webapp.domain;
+
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+//jpa allows for many relationships this example will use many to many
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    //set should be used vs a list, list will allow for duplicate items
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
